81d204716e1135d59b4fe4c2ce0d26b00c63d923
mokbhai <mokshitjain18@gmail.com>
2024-10-22 17:09:55 +0530

    event

--- src/apiconfig.js
+++ src/apiconfig.js
@@ -1,6 +1,7 @@
 // const URL = "https://techsprint-lpu.vercel.app";
 // const URL = "https://inovation.up.railway.app";
-const URL = "http://localhost:4000"
+// const URL = "http://localhost:4000";
 // const URL = "http://172.21.165.170:4000";
+const URL = "";
 
 export default URL;
